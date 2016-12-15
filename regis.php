f1a9f902e75251b231023a46e324ac8dd2995725
Brandon-Medrano <ing.brandon.medrano@gmail.com>
2016-12-14 23:02:27 -0600

    hg

--- regis.php
+++ regis.php
@@ -1,47 +1,19 @@
 
 <!DOCTYPE html>
 <html lang="es-ES">
-     
     <head>
-        
     <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/materialize.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    
         <title>bmmsystem</title>
-
-
     </head>
-
-
 <?php
-
-
 include("conex.php");
-
-
-
-
-
-
-
-
 ?>
-
     <body>
-
-
 <div class="card-panel blue z-depth-4">
-
-
-
 </div>
-
-           
     </body>
-
-
-
         <footer>Founder bmmsystem</footer>
 </head>
